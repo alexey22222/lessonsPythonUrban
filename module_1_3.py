b8c5d952305106d9292dd861512c9f907eef5b9b
alexey22222 <126365077+alexey22222@users.noreply.github.com>
2024-11-23 00:09:42 +0800

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = "Alexey"
+print("Name: " + name)
+age = 44
+print("Age: " + str(age))
+age = age + 5
+print("New Age: " + str(age))
+is_student = True
+print("Is Student: " + str(is_student))
+
