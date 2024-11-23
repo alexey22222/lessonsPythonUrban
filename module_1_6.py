e981ec07b70a09d0ed49b78de9d605b85a9d1b41
alexey22222 <126365077+alexey22222@users.noreply.github.com>
2024-11-23 23:21:38 +0800

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+amount_homework = 12
+amount_hours = 1.5
+course_name = 'Python'
+time_one_assignment = amount_hours / amount_homework
+print("Курс: " + course_name + ", всего задач: " + str(amount_homework) + ", затрачено часов: " + str(amount_hours) + ", среднее время выполнения " + str(time_one_assignment) + " часа.")
