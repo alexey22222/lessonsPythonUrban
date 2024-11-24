0d7e63694f4f857bd46fd8ca3e29f3164109da49
Alexey Kaygorodtsev <alexey22222@gmail.com>
2024-11-24 14:31:32 +0800

    add new module_1_9.py

---
+++ module_1_9.py
@@ -0,0 +1,7 @@
+example = 'My name is Alexey'
+print(example[0])
+print(example[-1])
+number_half_example = int(len(example)/2)
+print(example[number_half_example:])
+print(example[::-1])
+print(example[1::2])
