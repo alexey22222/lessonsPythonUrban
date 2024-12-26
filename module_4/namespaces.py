4046ac30eed874073c1d745903c9ea0ec7450683
Alexey Kaygorodtsev <alexey22222@gmail.com>
2024-12-27 00:17:55 +0800

    edit namespaces.py

--- module_4/namespaces.py
+++ module_4/namespaces.py
@@ -3,5 +3,5 @@ def test_function():
         print('Я в области видимости функции test_function')
     inner_function()
 
-inner_function() # данный вызов функции порождает ошибку,
+# inner_function() # данный вызов функции порождает ошибку,
                  # область видимости для inner_function() только внутри функции test_function
