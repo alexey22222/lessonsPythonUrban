de94ba2c56d5ea7df13d8191ac44b564d4d46505
Alexey Kaygorodtsev <alexey22222@gmail.com>
2025-01-02 10:44:51 +0800

    add module_5/magic_method.py, correct module_5/object_attribute.py

--- module_5/magic_method.py
+++ module_5/magic_method.py
@@ -8,9 +8,18 @@ class House:
                 print(i)
         else:
             print('"Такого этажа не существует"')
+    def __len__(self):
+        return  self.number_od_floors
+    def __str__(self):
+        return f'Название {self.name}, количество этажей: {self.number_od_floors}'
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
 
-h1.go_to(5)
-h2.go_to(10)
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
